704b9c2378e8b5073a06fb6bb0b153befe6a3b09
nikola <nikola.mitrovic.147.14@ict.edu.rs>
2019-10-08 22:43:15 +0200

    adding exception handler

---
+++ src/Core/ExceptionHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Core;
+
+use Throwable;
+
+class ExceptionHandler extends \Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function handle()
+    {
+        $this->getMessage();
+        $path_to_log_file = dirname(__FILE__,3).'/var/logs/error.log.txt';
+        $date = new \DateTime();
+        $date = date_format($date,"d/m/Y H:i:s");
+        file_put_contents($path_to_log_file, '[' . $date . '] ' . $this->getMessage() . ' ' . $this->getFile() . ' on line ' . $this->getLine() ."\n",FILE_APPEND | LOCK_EX);
+    }
+}
