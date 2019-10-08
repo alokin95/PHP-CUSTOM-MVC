d002acf86f9e698df8a1077b7250ea342d189f12
nikola <nikola.mitrovic.147.14@ict.edu.rs>
2019-10-08 22:43:28 +0200

    url validation

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -12,18 +12,25 @@ class Router
 
     public function get($route = '', array $params = [])
     {
-        $this->routes['GET'][$route] = [
-            'controller' => $params['controller'],
-            'action'    => $params['action']
-        ];
+
+        if ($this->validate_route($route))
+        {
+            $this->routes['GET'][$route] = [
+                'controller'    => $params['controller'],
+                'action'        => $params['action']
+            ];
+        }
     }
 
     public function post($route = '', array $params = [])
     {
-        $this->routes['POST'][$route] = [
-            'controller' => $params['controller'],
-            'action'    => $params['action']
-        ];
+        if ($this->validate_route($route))
+        {
+            $this->routes['POST'][$route] = [
+                'controller' => $params['controller'],
+                'action'    => $params['action']
+            ];
+        }
     }
 
     public function resolve($method, $uri)
@@ -34,6 +41,26 @@ class Router
             $controller_action = $this->routes[$method][$uri]['action'];
             $controller = new $controller;
             $controller->$controller_action();
+        }
+    }
+
+    private function validate_route($route)
+    {
+        try
+        {
+            $route = trim($route, '/');
+
+            $allowed_url_format = "/^([A-z0-9]{1,}|\{[A-z]{1,}\})(\/[A-z0-9]{1,}|\/\{[A-z]{1,}\})*([A-z0-9]{1,}|\/\{[A-z]{1,}\})*$/";
+
+            if (preg_match($allowed_url_format, $route))
+            {
+                return true;
+            }
+            throw new ExceptionHandler('Invalid route format');
+
+        }catch(ExceptionHandler $e)
+        {
+            $e->handle();
         }
     }
 }
