08b78c851dd0407f77402df35cec402fd76001fe
nikola <nikola.mitrovic.147.14@ict.edu.rs>
2019-11-07 00:27:23 +0100

    json method and render method

--- src/Core/Response.php
+++ src/Core/Response.php
@@ -6,13 +6,19 @@ namespace App\Core;
 
 class Response
 {
-    public function json()
+    public function json($data = [], $statusCode = 200)
     {
-
+        header('Content-Type: application/json');
+        http_response_code($statusCode);
+        echo json_encode([
+            'data'      => $data,
+            'status'    => $statusCode
+        ]);
     }
 
-    public function render()
+    public function render($view, $data = [])
     {
-
+        extract($data);
+        return require __DIR__.'/../../templates/'.$view;
     }
 }
