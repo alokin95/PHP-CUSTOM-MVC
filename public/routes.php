c4508cf6950139652fc8950ae62e551ad5db72f9
nikola <nikola.mitrovic.147.14@ict.edu.rs>
2019-11-08 00:14:58 +0100

    adding login route

--- public/routes.php
+++ public/routes.php
@@ -3,4 +3,4 @@
 $router->get('/', 'FrontController', 'index');
 $router->get('posts', 'PostController', 'index');
 $router->get('posts/{post}', 'PostController', 'show');
-$router->post('users', 'PostController', 'store');
+$router->post('login', 'AuthController', 'login');
